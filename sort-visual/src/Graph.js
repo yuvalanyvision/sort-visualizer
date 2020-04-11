737c3ccca4e1160b18c102b5af57a45921b70189
raziel <razielron@gmail.com>
2020-04-11 22:52:32 +0300

    hii

--- sort-visual/src/Graph.js
+++ sort-visual/src/Graph.js
@@ -9,7 +9,7 @@ class Graph extends React.Component {
         super(props);
 
         this.state = {
-            data: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19],
+            data: [...Array(50).keys()],
             algorithms: ['Default', 'BUBBLE', 'MERGE'],
         };
 
