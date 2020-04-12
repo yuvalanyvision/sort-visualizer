2ae5e264492f680e09e0be45228358d83c6d0b38
raziel <razielron@gmail.com>
2020-04-12 11:08:30 +0300

    hii

--- sort-visual/src/Graph.js
+++ sort-visual/src/Graph.js
@@ -61,7 +61,7 @@ async function postData(url = '', data = {}) {
     // Default options are marked with *
     const response = await fetch(url, {
         method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        mode: 'no-cors', // no-cors, *cors, same-origin
+        mode: 'same-origin', // no-cors, *cors, same-origin
         cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
         credentials: 'same-origin', // include, *same-origin, omit
         headers: {
